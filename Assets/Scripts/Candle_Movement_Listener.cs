7e8390f653ee2a296c375f62427256ea4cb03776
379146 <379146@colum.edu>
2021-02-11 17:20:41 -0600

    Dwight_Candle_Demo
    Added dripping and moving animation

---
+++ Assets/Scripts/Candle_Movement_Listener.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEditor.Animations;
+
+public class Candle_Movement_Listener : MonoBehaviour
+{
+
+    bool isMoving = false;
+    public Animator AnimController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimController = GetComponent<Animator>();
+        AnimController.SetBool("Is_Moving", false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            AnimController.SetBool("Is_Moving", true);
+        }
+        else
+        {
+            AnimController.SetBool("Is_Moving", false);
+        }
+
+    }
+}
