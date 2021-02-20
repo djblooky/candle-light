886ab984c1fe0f7ac5f15c18174e327aadafa155
Charlie Phillips <hi.charliephillips@gmail.com>
2021-02-19 19:02:42 -0600

    equipment renders on top layer

---
+++ Assets/Scripts/UpdateFOV.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateFOV : MonoBehaviour
+{
+
+    Camera thisCamera;
+
+    [SerializeField]
+    [Tooltip ("This is the camera whose FOV you want to match")]
+    Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCamera = this.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisCamera.fieldOfView = mainCamera.fieldOfView;
+    }
+}
