1a7f1db1ec1736383ed1a344f4bcddb6bba2ccb0
379146 <379146@colum.edu>
2021-02-26 14:04:26 -0600

    Dwight spooky scary trigger time
    Visual tracking and physical triggers

---
+++ Assets/Scenes/DwightTestScenes/DestroyTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DestroyTest : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (INTHAZONE && ISLOOKING)
+        {
+            Invoke("DestroyTime", VanishTime);
+        }
+
+    }
+
+    private bool INTHAZONE = false;
+    private bool ISLOOKING = false;
+    [SerializeField] private float VanishTime = 0.2f;
+
+    // Disable the behaviour when it becomes invisible...
+    void OnBecameInvisible()
+    {
+        ISLOOKING = false;
+    }
+
+    // ...and enable it again when it becomes visible.
+    void OnBecameVisible()
+    {
+        ISLOOKING = true;
+    }
+
+    void DestroyTime()
+    {
+
+        Destroy(gameObject);
+
+    }
+
+    private void inzone()
+    {
+        //Debug.Log("IN THA ZONE");
+        INTHAZONE = true;
+
+    }
+
+    private void OnEnable()
+    {
+        TriggerTest.Jump1 += inzone;
+    }
+
+    private void OnDisable()
+    {
+        TriggerTest.Jump1 -= inzone;
+    }
+
+}
---
+++ Assets/Scenes/DwightTestScenes/PeripheralVisionTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PeripheralVisionTest : MonoBehaviour
+{
+    // Update is called once per frame
+    void Start()
+    {
+        Child = transform.Find("mannequin_01 (1)");
+    }
+
+    private Transform Child;
+    private bool ISLOOKING = false;
+    [SerializeField] private float VanishTime = 0.1f;
+
+    // Disable the behaviour when it becomes invisible...
+    void OnBecameInvisible()
+    {
+        Debug.Log("invis");
+        Child.localScale = new Vector3(0f, 0f, 0f);
+
+    }
+
+    // ...and enable it again when it becomes visible.
+    void OnBecameVisible()
+    {
+        Debug.Log("VISIBLE");
+        Child.localScale = new Vector3(2.71135f, 0.7335142f, 2.859676f);
+        Invoke("VanishMode", VanishTime);
+
+    }
+
+    void VanishMode()
+    {
+        
+        Debug.Log("VANISH");
+        Child.localScale = new Vector3(0f,0f,0f);
+
+    }
+
+}
