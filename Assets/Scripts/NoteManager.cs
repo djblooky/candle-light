4d75d058a1db9c8fcb197a8ef14132a4d228eaf9
Charlie Phillips <hi.charliephillips@gmail.com>
2021-03-04 18:34:47 -0600

    note burns when closed

--- Assets/Scripts/NoteManager.cs
+++ Assets/Scripts/NoteManager.cs
@@ -1,17 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class NoteManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<GameObject> notes;
+
+    private void Start()
+    {
+
+    }
+
+    void OnRespawn()
+    {
+
+    }
+
+    private void OnEnable()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
         
     }
