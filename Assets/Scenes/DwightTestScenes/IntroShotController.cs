66e77a0f374d52f3692011425a9fd6a121b2d32a
379146 <379146@colum.edu>
2021-03-05 01:27:23 -0600

    Intro done needs audio work

---
+++ Assets/Scenes/DwightTestScenes/IntroShotController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroShotController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SwitchScenes", 20f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SwitchScenes()
+    {
+
+        SceneManager.LoadScene("Prototype V2");
+    
+    }
+}
