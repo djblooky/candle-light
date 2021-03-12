a01bd5f7bb8829bf238466a2310539bb45aa4793
BagboBillbins <aaron.baer@loop.colum.edu>
2021-03-11 20:33:05 -0600

    Added new intro and menu music.

---
+++ Assets/MenuMusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //AkSoundEngine.SetState("Music_Switch", "Menu");
+        //AkSoundEngine.PostEvent("Music_Switch", gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
