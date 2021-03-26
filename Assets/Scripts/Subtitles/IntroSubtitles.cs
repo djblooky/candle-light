1acf891e0b77a5771b513ca828539f32a7ff972f
Charlie Phillips <hi.charliephillips@gmail.com>
2021-03-26 01:03:23 -0500

    intro subtitles

---
+++ Assets/Scripts/Subtitles/IntroSubtitles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class IntroSubtitles : Subtitles
+{
+    protected override IEnumerator SubSequence()
+    {
+        yield return new WaitForSeconds(4);
+        textBox.text = "How long have I been trapped here?";
+        yield return new WaitForSeconds(3.5f);
+        textBox.text = "How long have I been wandering in this suffocating darkness with nothing but a flicker to sustain me?";
+        yield return new WaitForSeconds(6);
+        textBox.text = "It seems that I have no choice but to continue on and hope that the cycle of suffering will end.";
+        yield return new WaitForSeconds(6);
+        textBox.text = "This place, as strange as my cage is, the walls hold a sort of familiarity to them...";
+        yield return new WaitForSeconds(11);
+        textBox.text = "I have seen this place ennumerable times and yet...";
+        yield return new WaitForSeconds(4);
+        textBox.text = "It is ever so different.";
+    }
+}
---
+++ Assets/Scripts/Subtitles/Subtitles.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Subtitles : MonoBehaviour
+{
+    [SerializeField] protected TMP_Text textBox;
+    [SerializeField] private TMP_Text settingsButtonText; //TODO: add settings toggle for subtitles
+    [SerializeField] private bool areSubtitlesOn = false;
+
+    private void Start()
+    {
+        StartCoroutine(SubSequence());
+    }
+
+    protected virtual IEnumerator SubSequence()
+    {
+        return null;
+    }
+
+    /// <summary>
+    /// An event to link to a settings button
+    /// </summary>
+    public void SwitchSubtitles()
+    {
+        if (areSubtitlesOn)
+        {
+            textBox.enabled = false;
+            areSubtitlesOn = false;
+            settingsButtonText.text = "Turn On Subtitles";
+            return;
+        }
+        else
+        {
+            textBox.enabled = true;
+            areSubtitlesOn = true;
+            settingsButtonText.text = "Turn Off Subtitles";
+            return;
+        }
+    }
+}
