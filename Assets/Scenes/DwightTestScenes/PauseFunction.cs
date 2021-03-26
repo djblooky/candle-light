6de45b152368846d1c36df0d2f22a2203605db2d
379146 <379146@colum.edu>
2021-03-26 17:50:51 -0500

    pause testing

---
+++ Assets/Scenes/DwightTestScenes/PauseFunction.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseFunction : MonoBehaviour
+{
+    public GameObject pause;
+    public bool check = true;
+    public GameObject playerCon;
+    public RushCharacterController Rcc;
+
+    void Start()
+    {
+
+        Rcc = playerCon.GetComponent<RushCharacterController>();
+    
+    }
+
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.Escape)) && (check == false))
+        {
+            OpenPauseMenu();
+            Debug.Log("tick on ");
+        }
+        else if ((Input.GetKeyDown(KeyCode.Escape)) && (check == true))
+        {
+            ClosePauseMenu();
+            Debug.Log("tick off");
+        }
+    }
+
+    public void OpenPauseMenu()
+    {
+        pause.SetActive(true);
+        Time.timeScale = 0f;
+        Rcc.lockCamera = true;
+        check = true;
+    }
+
+    public void ClosePauseMenu()
+    {
+        pause.SetActive(false);
+        Time.timeScale = 1f;
+        Rcc.lockCamera = false;
+        check = false;
+
+    }
+
+    public void ReturnToMenu()
+    {
+
+        SceneManager.LoadScene(0);
+
+    }
+
+}
